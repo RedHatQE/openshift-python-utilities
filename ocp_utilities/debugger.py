b56f77d4eb9026eda6cf81c1918a4758852b2135
Meni Yakove <myakove@gmail.com>
2022-11-14 13:07:38 +0200

    Docstring update

--- ocp_utilities/debugger.py
+++ ocp_utilities/debugger.py
@@ -6,6 +6,8 @@ class WebDebugger(WebPdb):
     Project home:
         https://github.com/romanvm/python-web-pdb
 
+    port 1234 needs to be export in the docker container
+
     Usage:
         pytest --pdbcls=ocp_utilities.debugger:WebDebugger --pdb
     """
